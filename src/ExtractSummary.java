2de588ccb24f445824eb0820fcd816aad7a5c4c8
Ezra Ford <ezraford44@gmail.com>
2023-04-24 01:45:59 -0700

    Initial commit

---
+++ src/ExtractSummary.java
@@ -0,0 +1,24 @@
+
+// Final Project
+// Tal Mordokh, Ezra Ford, Chris Dennis
+
+import java.util.HashMap;
+
+public class ExtractSummary {
+    protected HashMap<String, Double>
+
+    /**
+     * Constructor
+     * @param inputFile input filepath
+     * @param outputFile output filepath
+     */
+    public ExtractSummary (String inputFile, String outputFile) {
+        /*
+        Split the article into sentences, then preprocess the text by lowercasing,
+        removing stop words, numbers, punctuation, and other special characters
+        from the sentences
+         */
+    }
+
+
+}
